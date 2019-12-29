91fbc89c44ed47908d80984b7265be1e597588f2
Sylvain Lesage <severo@rednegra.net>
2019-12-29 19:23:46 +0100

    feat: check domains, page style

--- src/index.js
+++ src/index.js
@@ -1,22 +1,6 @@
-// const whois = require('whois-api')
-// const dsv = require('d3-dsv')
+const whois = require('whois-api')
 const fs = require('fs').promises
 const Mustache = require('mustache')
-
-const parseCsv = text => {
-  const rows = [{
-    domain: 'rezo.net',
-    name: 'Rezo',
-    description: 'Le réseau des copains',
-    url: 'https://rezo.net'
-  }, {
-    domain: 'visionscarto.net',
-    name: 'Visionscarto',
-    description: 'Réseau indépendant de recherche sur les cartes et les représentations',
-    url: 'https://visionscarto.net'
-  }]
-  return rows
-}
 
 const addDate = async rows => {
   const rowsOK = []
@@ -28,42 +12,47 @@ const addDate = async rows => {
 }
 
 const getClass = days => {
-  return (days < 0)
-    ? 'past' : (days < 10)
-      ? 'urgent' : (days < 30)
-        ? 'soon' : 'ok'
+  return days < 0 ? 'past' : days < 90 ? 'soon' : 'ok'
+}
+
+const lookup = async domain => {
+  return new Promise((resolve, reject) => {
+    // to be promisified
+    whois.lookup(domain, function (err, data) {
+      if (err) {
+        reject(err)
+      }
+      if (data.registrar !== 'Gandi SAS') {
+        reject(new RangeError('For now, the only supported registrar is Gandi'))
+      }
+      resolve(data.expiration_date)
+    })
+  })
 }
 
 const addDateToRow = async row => {
-  // to be promisified
-  // whois.lookup(row.domain, function (err, data) {
-  //   if (err) {
-  //     data.expiration_date = '2020-03-07T05:00:00Z'
-  //   }
-  //   // 2023-05-20T14:01:45Z
-  //   row.expiration_date = data.expiration_date || '2020-03-07T05:00:00Z'
-  // })
-  if (row.domain === 'rezo.net') {
-    row.expirationDate = '2020-03-07T05:00:00Z'
-  } else {
-    row.expirationDate = '2023-05-20T14:01:45Z'
-  }
-  row.daysLeft = Math.floor((new Date(row.expirationDate) - Date.now()) / (1000 * 60 * 60 * 24))
+  row.expirationDate = await lookup(row.domain)
+  row.daysLeft = Math.floor(
+    (new Date(row.expirationDate) - Date.now()) / (1000 * 60 * 60 * 24)
+  )
   row.class = getClass(row.daysLeft)
   return row
 }
 
 const writeHtml = (html, filename) => {
-  fs.writeFile(filename, html).then(() => console.log('The file was saved!')).catch(console.log)
+  fs.writeFile(filename, html)
+    .then(() => console.log('The file was saved!'))
+    .catch(console.log)
 }
 
 const run = async () => {
-  const text = await fs.readFile('src/domains.csv', 'utf8')
-  const rows = await parseCsv(text)
-  // const rows = dsv.parse(text)
-  const rowsOK = await addDate(rows)
+  const text = await fs.readFile('src/domains.json', 'utf8')
+  const domains = JSON.parse(text)
+  const domainsOK = await addDate(domains)
   const template = await fs.readFile('src/index.mustache', 'utf8')
-  const html = Mustache.render(template, { domains: rowsOK.sort((a, b) => a.daysLeft > b.daysLeft) })
+  const html = Mustache.render(template, {
+    domains: domainsOK.sort((a, b) => a.daysLeft > b.daysLeft)
+  })
   writeHtml(html, 'public/index.html')
 }
 
